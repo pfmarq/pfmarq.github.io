5bf3cfbf58886b46b4fe077612138fb2b138a556
CMarah <carlos.maria_rodriguez@hotmail.com>
2019-08-23 20:54:20 +0200

    First version without silly stuff

--- code.js
+++ code.js
@@ -1,8 +1,16 @@
 const TABS = ['gal_tab', 'cv_tab', 'me_tab'];
 const BTNS = ['gal_btn', 'cv_btn', 'me_btn'];
 changeTab = new_tab => {
-  TABS.forEach(tab => document.getElementById(tab).style.display = 'none');
+  TABS.forEach(tab => {
+    if (document.getElementById(tab)) {
+      document.getElementById(tab).style.display = 'none';
+    }
+  });
   document.getElementById(new_tab + '_tab').style.display = '';
-  BTNS.forEach(btn => document.getElementById(btn).style.fontWeight = '');
+  BTNS.forEach(btn => {
+    if (document.getElementById(btn)) {
+     document.getElementById(btn).style.fontWeight = '';
+    }
+  });
   document.getElementById(new_tab + '_btn').style.fontWeight = 700;
 }
